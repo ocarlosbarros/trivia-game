68a769d71490d0c0987a813ba12ae3f4ad7aa0d7
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-24 17:29:26 -0300

    Adiciona estrutura de Header para ler o state com redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './Header.css';
 
 class Header extends Component {
   render() {
+    const { player: { name, path, score } } = this.props;
     return (
       <header className="header">
         <div>
-          <img data-testId="header-profile-picture" src="" alt="" />
+          <img data-testId="header-profile-picture" src={ path } alt="" />
         </div>
-        <p data-testId="header-player-name" />
-        <p data-testId="header-score" />
+        <p data-testId="header-player-name">{name}</p>
+        <p data-testId="header-score">{ score }</p>
       </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  player: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  player: state.player.player,
+});
+
+export default connect(mapStateToProps, null)(Header);
