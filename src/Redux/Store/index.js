a7b5304b0ffe91720e5a945943cebb4d1c670ed4
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-24 16:39:33 -0300

    Adiciona configuracao do thunk

--- src/Redux/Store/index.js
+++ src/Redux/Store/index.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducers from '../Reduces';
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 if (window.Cypress) {
   window.store = store;
