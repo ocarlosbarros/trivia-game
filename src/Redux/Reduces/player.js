1b4abd1f91e1f415f14834c95518e6d32d079bab
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-24 20:22:01 -0300

    Faz login e salva dados no estado global da aplicao e busca dados do estado global para componente Header

--- src/Redux/Reduces/player.js
+++ src/Redux/Reduces/player.js
@@ -1,16 +1,21 @@
-import { GET_PLAYER } from '../../actions/getPlayer';
+import { GET_PLAYER, LOGIN } from '../../Actions';
 
 const INITIAL_STATE = {
-  player: {
-    name: '',
-    assertions: 0,
-    score: 0,
-    gravatarEmail: '',
-  },
+  name: '',
+  assertions: 0,
+  score: 0,
+  gravatarEmail: '',
 };
 
 function player(state = INITIAL_STATE, action) {
+  console.log(action.payload);
   switch (action.type) {
+  case LOGIN:
+    return {
+      ...state,
+      name: action.payload.name,
+      gravatarEmail: action.payload.email,
+    };
   case GET_PLAYER:
     return { ...state };
   default:
--- src/actions/getPlayer.js
+++
@@ -1,5 +0,0 @@
-const GET_PLAYER = 'GET_PLAYER';
-
-const getPlayerAction = (player) => ({ type: GET_PLAYER, payload: player });
-
-export { getPlayerAction, GET_PLAYER };
