fa1545f68ebb0c4bfa01d474930c409f14eacddb
neandermenezes <neander2801@gmail.com>
2021-11-25 17:39:55 -0300

    nada

--- src/ReactPages/Login.jsx
+++ src/ReactPages/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { loginAction } from '../Actions';
+import { loginAction } from '../Redux/Actions';
 
 class Login extends React.Component {
   constructor(props) {
--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -1,13 +1,44 @@
+import fetchGravatarImage from '../../services/fetchGravatarImage';
 import getAnswers from '../../services/getAnswers';
 
-export const GET_ANSWERS = 'GET_ANSWERS';
+const GET_ANSWERS = 'GET_ANSWERS';
 
-export const actionAnswers = (answers) => ({
+const actionAnswers = (answers) => ({
   type: GET_ANSWERS,
   answers,
 });
 
-export const actionGetAnswers = (token) => async (dispatch) => {
+const actionGetAnswers = (token) => async (dispatch) => {
   const answers = await getAnswers(token);
   dispatch(actionAnswers(answers));
+};
+
+const GET_PLAYER = 'GET_PLAYER';
+const GET_GRAVATAR_IMAGE = 'GET_GRAVATAR_IMAGE';
+const LOGIN = 'LOGIN';
+const getPlayerAction = (player) => ({ type: GET_PLAYER, payload: player });
+
+const loginAction = (player) => ({ type: LOGIN, payload: player });
+
+const getGravatarImage = (imagePath) => (
+  { type: GET_GRAVATAR_IMAGE, payload: imagePath });
+
+function getGravatarImageAction(email) {
+  return (dispatch) => {
+    fetchGravatarImage(email)
+      .then((imagePath) => dispatch(getGravatarImage(imagePath)))
+      .catch((error) => console.log(error));
+  };
+}
+
+export {
+  getPlayerAction,
+  GET_PLAYER,
+  loginAction,
+  LOGIN,
+  getGravatarImageAction,
+  GET_GRAVATAR_IMAGE,
+  actionGetAnswers,
+  actionAnswers,
+  GET_ANSWERS,
 };
