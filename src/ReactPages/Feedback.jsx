065a633d75677ff161798915c098ae16010747df
neandermenezes <neander2801@gmail.com>
2021-11-30 17:17:14 -0300

    requisito 13

--- src/ReactPages/Feedback.jsx
+++ src/ReactPages/Feedback.jsx
@@ -3,14 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import '../css/Feedback.css';
 
+const minAssertions = 3;
+
 class Feedback extends React.Component {
   render() {
-    const { img, name, score } = this.props;
+    const { img, name, score, assertions } = this.props;
     return (
       <div className="feedback-section">
-        <img data-testid="header-profile-picture" src={ img } alt="user profile" />
-        <p data-testid="header-player-name">{ name }</p>
-        <p data-testid="header-score">{ score }</p>
+        <img
+          data-testid="header-profile-picture"
+          src={ img }
+          alt="user profile"
+        />
+        <p data-testid="header-player-name">{name}</p>
+        <p data-testid="header-score">{score}</p>
+        <p data-testid="feedback-text">
+          {assertions < minAssertions ? 'Podia ser melhor...' : 'Mandou bem!'}
+        </p>
       </div>
     );
   }
@@ -20,12 +29,14 @@ const mapStateTopProps = ({ players }) => ({
   img: players.imagePath,
   name: players.name,
   score: players.score,
+  assertions: players.assertions,
 });
 
 Feedback.propTypes = {
   img: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateTopProps)(Feedback);
