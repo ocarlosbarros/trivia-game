0568ff33571bf7d5e05befde5f5382ae803bd8bc
neandermenezes <neander2801@gmail.com>
2021-11-30 17:26:45 -0300

    req 12 ao 15

--- src/ReactPages/Feedback.jsx
+++ src/ReactPages/Feedback.jsx
@@ -2,41 +2,51 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import '../css/Feedback.css';
+import Header from '../ReactComponents/Header';
 
 const minAssertions = 3;
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+
+    this.redirect = this.redirect.bind(this);
+  }
+
+  redirect() {
+    const { history } = this.props;
+    history.push('/game');
+  }
+
   render() {
-    const { img, name, score, assertions } = this.props;
+    const { score, assertions } = this.props;
     return (
       <div className="feedback-section">
-        <img
-          data-testid="header-profile-picture"
-          src={ img }
-          alt="user profile"
-        />
-        <p data-testid="header-player-name">{name}</p>
-        <p data-testid="header-score">{score}</p>
+        <Header />
         <p data-testid="feedback-text">
           {assertions < minAssertions ? 'Podia ser melhor...' : 'Mandou bem!'}
         </p>
+        <p data-testid="feedback-total-score">{score}</p>
+        <p data-testid="feedback-total-question">{assertions}</p>
+        <button type="button" onClick={ this.redirect } data-testid="btn-play-again">
+          Jogar novamente
+        </button>
       </div>
     );
   }
 }
 
 const mapStateTopProps = ({ players }) => ({
-  img: players.imagePath,
-  name: players.name,
   score: players.score,
   assertions: players.assertions,
 });
 
 Feedback.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   assertions: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 export default connect(mapStateTopProps)(Feedback);
